38ec6456ee041b0a3bf9bd6ca037ebbed0040798
pieszynski <git@pieszynski.com>
2017-11-23 19:47:40 +0100

    Wycinanie dla konkretnego adresu
    Możliwość wycinania ruchu tylko jeśli w zakładce umieszczono pasujący
    url

--- rachCiach/ciach.js
+++ rachCiach/ciach.js
@@ -1,18 +1,39 @@
 
+/*
+DOC
+linia w konfiguracji może wyglądać tak:
+
+    jakis_tam|regex|[123]
+    {"rex":"jakis_tam|regex|[123]", "path":"regex_url|paska[adresu]"}
+
+*/
+
 var urlFilter = {
     urls: [ '<all_urls>' ]
 };
 var opts = [ 'blocking' ];
 
 var rex = [];
+var tabInf = [];
 
 restoreOptions();
 chrome.runtime.onMessage.addListener ( messageReceived );
 chrome.webRequest.onBeforeRequest.addListener( blockAction, urlFilter, opts );
+// tabs
+chrome.tabs.query({}, getInitialTabs);
+chrome.tabs.onCreated.addListener(onTabCreated);
+chrome.tabs.onUpdated.addListener(onTabUpdated);
+chrome.tabs.onRemoved.addListener(onTabRemoved);
 
 function blockAction ( details ) {
 
-    var cancel = rex.some ( function ( el ) { return el.test( details.url ); } );
+    var cancel = rex.some ( function ( el ) {
+        var tab = tabInf [ details.tabId ];
+        if ( el.path && tab ){
+            return el.path.test ( tab.url ) && el.rex.test( details.url );
+        }
+        return el.rex.test( details.url ); 
+    });
     return { 
         cancel: cancel
     };
@@ -24,7 +45,27 @@ function restoreOptions ( ) {
         ciach: [ ]
     }, function ( state ) {
 
-        rex = state.ciach.map ( function ( el ) { return new RegExp ( el, 'i' ); } );
+        rex = state.ciach
+        .filter( function ( el ) { return !( 0 === el.indexOf ( '#' ) ); } )
+        .map ( function ( el ) {
+            var obj = {
+                rex: el,
+                path: null
+            }
+            
+            if (0 === el.indexOf('{')) {
+                try {
+                    var jobj = JSON.parse(el);
+                    obj = jobj;
+                } catch (ex) {
+                    console.error('Config line starting with "{" means JSON in line', ex);
+                }
+            }
+            return {
+                rex: new RegExp ( obj.rex, 'i' ),
+                path: obj.path && new RegExp ( obj.path, 'i' )
+            };
+        });
     });
 }
 
@@ -32,4 +73,26 @@ function messageReceived ( message, sender, response ) {
 
     if ( message && message.update )
         restoreOptions();
+}
+
+function getInitialTabs(tabs) {
+    for(t of tabs) {
+        tabInf[t.id] = {
+            url: t.url
+        };
+    }
+}
+
+function onTabCreated(tab) {
+    tabInf[tab.id] = {
+        url: tab.url
+    };
+}
+
+function onTabUpdated(tabId, changeInfo,  tab) {
+    tabInf[tab.id].url = tab.url;
+}
+
+function onTabRemoved(tabId) {
+    tabInf.splice(tabId, 1);
 }
