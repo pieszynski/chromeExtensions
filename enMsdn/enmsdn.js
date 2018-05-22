76b4a6d75c2372f481e74635c5b4394797a984fd
pieszynski <git@pieszynski.com>
2018-05-22 18:48:46 +0200

    azure and docs

--- enMsdn/enmsdn.js
+++ enMsdn/enmsdn.js
@@ -1,19 +1,42 @@
 
-var regexMsdn = /^http[s]?(:\/\/msdn.microsoft.com\/(\w\w-\w\w)\/)/i,
-	regexMsdnReplace = /:\/\/msdn.microsoft.com\/\w\w-\w\w\//i,
-	enUsLocale = 'en-us',
-	enMsdnSite = '://msdn.microsoft.com/en-us/';
+// var regexMsdn = /^http[s]?(:\/\/msdn.microsoft.com\/(\w\w-\w\w)\/)/i,
+// 	regexMsdnReplace = /:\/\/msdn.microsoft.com\/\w\w-\w\w\//i,
+// 	enUsLocale = 'en-us',
+// 	enMsdnSite = '://msdn.microsoft.com/en-us/';
 
-chrome.webNavigation.onBeforeNavigate.addListener( function enmsdn_be4nav( details ) {
+// https://msdn.microsoft.com/pl-pl/library/system.threading.thread(v=vs.110).aspx
+// https://azure.microsoft.com/pl-pl/services/
+// https://docs.microsoft.com/pl-pl/dotnet/csharp/programming-guide/concepts/threading/
+// https://docs.microsoft.com/pl-pl/aspnet/core/fundamentals/static-files?view=aspnetcore-2.0&tabs=aspnetcore2x
 
-	var match = regexMsdn.exec ( details.url );
+var hosts = [
+	'msdn.microsoft.com',
+	'azure.microsoft.com',
+	'docs.microsoft.com'
+]
+
+hosts.forEach( function ( el ) { addHostListener ( el ); } );
+
+function addHostListener ( host ) {
+
+	var regexMsdn = new RegExp('^http[s]?(:\\/\\/' + host + '\\/(\\w\\w-\\w\\w)\\/)', 'i'),
+		regexMsdnReplace = new RegExp(':\\/\\/' + host + '\\/\\w\\w-\\w\\w\\/'),
+		enUsLocale = 'en-us',
+		enMsdnSite = '://' + host + '/en-us/';
+
+	console.debug( `Host redirection for site ${host} registered.` );
+
+	chrome.webNavigation.onBeforeNavigate.addListener( function enmsdn_be4nav( details ) {
+
+		var match = regexMsdn.exec ( details.url );
+		
+		if ( match && 3 === match.length && enUsLocale !== match[ 2 ].toLowerCase() ) {
 	
-	if ( match && 3 === match.length && enUsLocale !== match[ 2 ].toLowerCase() ) {
-
-		var newUrl = details.url.replace( regexMsdnReplace, enMsdnSite );
-
-		chrome.tabs.update( details.tabId, { url: newUrl } );
-	}
-},
-{ url: [ { hostEquals: 'msdn.microsoft.com' } ] }
-);
+			var newUrl = details.url.replace( regexMsdnReplace, enMsdnSite );
+	
+			chrome.tabs.update( details.tabId, { url: newUrl } );
+		}
+	},
+	{ url: [ { hostEquals: host } ] }
+	);
+}
